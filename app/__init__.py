7d31c3642e3cc020dde7f979ad5f5d0919288334
Guilherme Gonçalves <guilherme16.gon@gmail.com>
2023-04-12 15:06:19 -0300

    change database_url to internal connection

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
+
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 print('URL DATABASE', os.environ.get("DATABASE_URL"))
 app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
