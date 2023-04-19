f751e298365d3f0d3e8c26ddb392c7a1b306f62a
Guilherme Gonçalves <guilherme16.gon@gmail.com>
2023-04-19 00:56:36 -0300

    arrumando .env

--- app/__init__.py
+++ app/__init__.py
@@ -8,15 +8,14 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_SQLITE')
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite.db'
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 # Internal
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://controle_frequencia_user:a0hkpvsHOpBSoeXXaFjjHs04SGvsdpMa@dpg-cgrcrcrk9u56e3lod2l0-a/controle_frequencia'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://controle_frequencia_user:a0hkpvsHOpBSoeXXaFjjHs04SGvsdpMa@dpg-cgrcrcrk9u56e3lod2l0-a/controle_frequencia'
 # External
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://controle_frequencia_user:a0hkpvsHOpBSoeXXaFjjHs04SGvsdpMa@dpg-cgrcrcrk9u56e3lod2l0-a.oregon-postgres.render.com/controle_frequencia'
-print('URL DATABASE', os.environ.get("DATABASE_URL"))
-app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
